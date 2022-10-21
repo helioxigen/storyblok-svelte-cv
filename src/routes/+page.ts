fe4505b0905f75e18de159f463d5e7dd114de761
Alexander <al.zatulyviter@gmail.com>
2022-10-21 08:31:20 +0300

    Update +page.ts
    Set VITE env prefix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,7 @@ import { storyblokInit, apiPlugin } from '@storyblok/svelte';
 import { components } from '../components/storyblok';
 
 storyblokInit({
-	accessToken: process.env.STORYBLOK_TOKEN,
+	accessToken: process.env.VITE_STORYBLOK_TOKEN,
 	use: [apiPlugin],
 	components
 });
